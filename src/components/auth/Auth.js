41832170dfcbcb09251d9a949f7c1f921c248a08
mherzog <mherzog@hubspot.com>
2020-10-24 13:48:56 -0400

    Expose client id and prod uri

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 function Auth() {
   const scopes = "user-top-read playlist-modify-public streaming";
-  console.log(process.env.redirectURI)
   return (
     <>
-      <a href={`${process.env.REACT_APP_baseURL}?client_id=${process.env.REACT_APP_clientID}&response_type=${process.env.REACT_APP_responseType}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(process.env.REACT_APP_redirectURI)}`}>
+      <a href={`${process.env.baseURL}?client_id=${process.env.clientID}&response_type=${process.env.responseType}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent( process.env.NODE_ENV == "development" ? process.env.redirectURI : process.env.prodRedirectURI)}`}>
         Auth
       </a>
     </>
