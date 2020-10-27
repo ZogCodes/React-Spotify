7b2541572f95c6a4537991bf00df230fbfea326b
mherzog <mherzog@hubspot.com>
2020-10-27 17:08:53 -0400

    Tiny padding change

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -3,11 +3,11 @@ import React from 'react';
 function Auth() {
   const scopes = "user-top-read playlist-modify-public streaming";
   return (
-    <>
+    <section>
       <a href={`${process.env.baseURL}?client_id=${process.env.clientID}&response_type=${process.env.responseType}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent( process.env.NODE_ENV == "development" ? process.env.redirectURI : process.env.prodRedirectURI)}`}>
         Auth
       </a>
-    </>
+    </section>
   );
 }
 export default Auth;
