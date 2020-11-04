3f17fd58b85386efc97cbd0b10af6ac7ebdd1d3e
mherzog <mherzog@hubspot.com>
2020-11-04 12:17:33 -0500

    remove client secret

--- config.js
+++ config.js
@@ -2,7 +2,6 @@ const config = {
   clientID: "fd2908fa10804398880f6cbcf865bac9",
   redirectURI: "http://localhost:8080/build",
   scopes: "user-top-read playlist-modify-public streaming",
-  clientSecret: "425bc2b9f6464f919032ea1ff61809d0",
   prodRedirectURI: "https://zog-react-spotify.netlify.app/build"
 };
 
